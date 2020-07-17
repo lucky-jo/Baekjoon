54f676e0592fa13479d85879c1d0643d24d0777a
ildang100 <last2599@gmail.com>
2020-07-17 23:21:46 +0900

    A+B - 8 / 11022번
    문제
    두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 테스트 케이스의 개수 T가 주어진다.
    
    각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
    
    출력
    각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.

---
+++ src/backjoon/forloop/PrintCaseSum_2.java
@@ -0,0 +1,24 @@
+package backjoon.forloop;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class PrintCaseSum_2 {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try {
+            int n = Integer.parseInt(bufferedReader.readLine());
+            for (int i = 1; i <= n; i++ ) {
+                String[] temp = bufferedReader.readLine().split(" ");
+                bufferedWriter.write("Case #"+i+": "+Integer.parseInt(temp[0])+" + "+Integer.parseInt(temp[1])+" = "+(Integer.parseInt(temp[0])+Integer.parseInt(temp[1]))+"\n");
+            }
+            bufferedWriter.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
