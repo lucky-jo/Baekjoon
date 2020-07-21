72697d78929bfdf4f6e010a2a049c4da9a383407
ildang100 <last2599@gmail.com>
2020-07-21 23:41:19 +0900

    A/B / 1008번
    문제
    두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
    
    출력
    첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.

---
+++ src/backjoon/inOut/DivideAB.java
@@ -0,0 +1,14 @@
+package backjoon.inOut;
+
+import java.util.Scanner;
+
+public class DivideAB {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        System.out.println((double)(a/b));
+
+    }
+}
