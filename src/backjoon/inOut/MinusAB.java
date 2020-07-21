6fce4a5c47cf3e3828ad0d0d37fca03b01274517
ildang100 <last2599@gmail.com>
2020-07-21 23:40:12 +0900

    A-B / 1001 번
    문제
    두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
    
    출력
    첫째 줄에 A-B를 출력한다.

---
+++ src/backjoon/inOut/MinusAB.java
@@ -0,0 +1,14 @@
+package backjoon.inOut;
+
+import java.util.Scanner;
+
+public class MinusAB {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println( a - b );
+    }
+}
