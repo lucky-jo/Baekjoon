bc1a1498b66bcd36cb359c74487ebd32cec6f9db
ildang100 <last2599@gmail.com>
2020-07-21 23:36:55 +0900

    고양이 / 10171 번
    문제
    아래 예제와 같이 고양이를 출력하시오.
    
    입력
    없음.
    
    출력
    고양이를 출력한다.

---
+++ src/backjoon/inOut/Cat.java
@@ -0,0 +1,11 @@
+package backjoon.inOut;
+
+public class Cat {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
